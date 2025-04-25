9e6999c1a53993b786954cd314f2bd10bfbee172
Roman <romanjolsen@gmail.com>
2025-04-25 11:33:16 -0600

    added S to http

--- client/src/env.js
+++ client/src/env.js
@@ -1,5 +1,5 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:3000' : ''
+export const baseURL = dev ? 'https://localhost:7045' : ''
 export const useSockets = false
 export const domain = 'dev-1eliqcykgosr1ydc.us.auth0.com'
 export const clientId = 'JaEv0p3AivYE08AHm6OWUkQgLYkOK8hF'
