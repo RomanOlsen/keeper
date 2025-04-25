6f529e081b74c7a62a50eef6b6e447aa555a3011
Roman <romanjolsen@gmail.com>
2025-04-25 11:17:14 -0600

    init and filling out env and appsettings

---
+++ client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-1eliqcykgosr1ydc.us.auth0.com'
+export const clientId = 'JaEv0p3AivYE08AHm6OWUkQgLYkOK8hF'
+export const audience = 'https://auth.RomansFirstApp.com'
