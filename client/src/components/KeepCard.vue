8cbeb62c3c369937b44975291d33e5d05fc02768
Roman <romanjolsen@gmail.com>
2025-04-27 12:06:22 -0600

    masonry attempt

---
+++ client/src/components/KeepCard.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { Keep } from '@/models/Keep.js';
+
+defineProps({
+  keep: { type: Keep, required: true }
+})
+
+</script>
+
+
+<template>
+  <div class="keep" :style="{ backgroundImage: `url(${keep.img})` }">
+    <div class="card-header">{{ keep.name }}</div>
+    <!-- <div class="card-body"> -->
+      <!-- <img :src="keep.img" class="img-fluid"> -->
+    <!-- </div> -->
+    <!-- <div class="card-footer"></div> -->
+  </div>
+
+</template>
+
+
+<style lang="scss" scoped>
+.keep {
+  border: none;
+  background-size: cover;
+  // background-repeat: no-repeat;
+  background-position: center;
+  // width: 100%;
+  // min-height: 10dvh;
+  // display: inline;
+  // flex-grow: 1;
+  // size:0cap;
+  // flex-grow: 1;
+  // min-height: 30dvh;
+
+  // aspect-ratio: 1/1;
+}
+</style>
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -1,17 +1,21 @@
 <script setup>
+import { AppState } from '@/AppState.js';
+import KeepCard from '@/components/KeepCard.vue';
 import { keepsService } from '@/services/KeepsService.js';
 import { Pop } from '@/utils/Pop.js';
-import { onMounted } from 'vue';
+import { computed, onMounted } from 'vue';
 
-onMounted(()=>{
+onMounted(() => {
   getAllKeeps()
 })
+
+const keeps = computed(() => AppState.keeps);
 
 async function getAllKeeps() {
   try {
     await keepsService.getAllKeeps()
   }
-  catch (error){
+  catch (error) {
     Pop.error(error);
   }
 }
@@ -19,7 +23,22 @@ async function getAllKeeps() {
 </script>
 
 <template>
-  <div>Keeper Home</div>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div v-for="keep in keeps" :key="keep.id" class="masonry-roman">
+          <KeepCard :keep="keep" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.masonry-roman {
+  display: flex;
+  flex-flow: row wrap;
+  align-content: center;
+  // justify-content: space-evenly;
+}
+</style>
